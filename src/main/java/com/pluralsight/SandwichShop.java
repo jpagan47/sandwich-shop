47c13a954298c15cff30f69e6843f4642b1aadad
josepagan <josepaganrnj@outlook.com>
2026-04-12 20:48:17 -0400

    Added "Loaded" Cost to code. End of Code

--- src/main/java/com/pluralsight/SandwichShop.java
+++ src/main/java/com/pluralsight/SandwichShop.java
@@ -5,29 +5,48 @@ import java.util.Scanner;
 public class SandwichShop {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
+        //My variables
+        double totalPrice = 0;
+        //My Print Statements
         System.out.println("What size would you like the sandwich, (1) Regular or (2)Large? ");
-        int sandwichSize = myScanner.nextInt();
+        String theLine = myScanner.nextLine();
+        int sandwichSize = Integer.parseInt(theLine);
+
 
         System.out.println("What is your age? ");
-        int userAge = myScanner.nextInt();
+        theLine = myScanner.nextLine();
+        int userAge = Integer.parseInt(theLine);
 
 
-        double basePrice = 0;
-        if (sandwichSize == 1){
-            basePrice += 5.45;
+        System.out.println("Would you like to make it a loaded sandwich? ");
+        String makeItLoaded = myScanner.nextLine();
+
+
+        double loadCost = 0;
+
+        //Sandwich Size
+        if (sandwichSize == 1) {
+            totalPrice += 5.45;
+            loadCost = 1;
+        } else if (sandwichSize == 2) {
+            totalPrice += 8.95;
+            loadCost = 1.75;
         }
-        else if(sandwichSize == 2) {
-            basePrice += 8.95;
-        }
-        if(userAge <= 17){
-            basePrice *= .90;
-        }
-        else if (userAge >= 65) {
-            basePrice *= .80;
+        
+        // Loaded extra cost
+        if (makeItLoaded.equalsIgnoreCase("Yes")) {
+            totalPrice += loadCost;
         }
 
 
+        //Users Age
+        if (userAge <= 17) {
+            totalPrice *= .90;
+        } else if (userAge >= 65) {
+            totalPrice *= .80;
+        }
 
-        System.out.println("Your price is " + basePrice);
+
+        System.out.println("Your price is " + totalPrice);
     }
 }
